a28d284e2786eda4a71f4d3781cbb6aa67c5e85c
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-02-25 09:58:25 +0300

    change route

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/magic', methods=['POST'])
 def magic():
-    return ""
+    return json.dumps({
+        "hello": "test"
+    })
 
 
 if __name__ == "__main__":
