c37b5391ab68bf6f33dbb1c95a8c06f14ca590d2
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-02-26 22:54:34 +0300

    change some structure

--- app/neural.py
+++ app/neural.py
@@ -7,8 +7,6 @@ train_data_x = x[1:4]
 print(train_data_x)
 train_answers = y[1:4]
 num_epoch = 100
-w1, w2, w3 = 0, 0, 0
-coef = 1
 
 
 def activate_function_simple(net):
@@ -16,23 +14,29 @@ def activate_function_simple(net):
 
 
 def activate_function_hardly(net):
-    return 0.5 * ((net/(1 + fabs(net))) + 1)
+    return 0.5 * ((net / (1 + fabs(net))) + 1)
 
 
-for epoch in range(num_epoch):
-    for i in range(3):
-        print(epoch, i)
-        a = train_data_x[i][0] * w1 + train_data_x[i][1] * w2 + w3
-        y = activate_function_hardly(a)
-        print('data', train_data_x[i], y)
-        # TODO: check error
-        w1 = w1 + coef * (train_answers[i] - y) * y * (1 - y) * train_data_x[i][0]
-        w2 = w2 + coef * (train_answers[i] - y) * y * (1 - y) * train_data_x[i][1]
-        w3 = w3 + coef * (train_answers[i] - y) * y * (1 - y)
-        print('weights', w1, w2, w3)
+def training(w1, w2, w3):
+    coef = 0.3
 
+    for epoch in range(num_epoch):
+        for i in range(3):
+            print(epoch, i)
+            net = train_data_x[i][0] * w1 + train_data_x[i][1] * w2 + w3
+            y = activate_function_hardly(net)
+            print('data', train_data_x[i], y)
+            # TODO: check error
+            w1 = w1 + coef * (train_answers[i] - y) * y * (1 - y) * train_data_x[i][0]
+            w2 = w2 + coef * (train_answers[i] - y) * y * (1 - y) * train_data_x[i][1]
+            w3 = w3 + coef * (train_answers[i] - y) * y * (1 - y)
+            print('weights', w1, w2, w3)
+
+
+print('Training...')
+training(0, 0, 0, )
 print('Done')
 
 print('Test')
-a = x[3][0] * w1 + x[3][1] * w2 + w3
-print(activate_function_hardly(a))
+net = x[3][0] * w1 + x[3][1] * w2 + w3
+print(activate_function_hardly(net))
