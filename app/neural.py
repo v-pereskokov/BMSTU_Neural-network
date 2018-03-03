c1fc3e532069845a90704e290cea36a2a8241d34
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-03-03 07:54:23 +0300

    add weights and vars

--- app/neural.py
+++ app/neural.py
@@ -9,17 +9,27 @@ train_answers = y[1:4]
 
 
 class BooleanNeural:
+    variables = None
+    model_function = None
     activate_function = None
     training_nu = None
     epoch_number = None
-    w1, w2, w3 = 0, 0, 0
+    weights = []
 
-    def __init__(self, activate_function, training_nu=1.0, epoch_number=100):
+    def __init__(self, variables, model_function, activate_function, training_nu=1.0, epoch_number=100):
+        self.variables = variables
+        self.model_function = model_function
         self.activate_function = activate_function
         self.training_nu = training_nu
         self.epoch_number = epoch_number
 
     def training(self):
+        for step in range(2 ** self.variables):
+            self.weights[step] = 0
+
+        return self.__training__()
+
+    def __training__(self):
         for epoch in range(self.epoch_number):
             for i in range(3):
                 net = train_data_x[i][0] * self.w1 + train_data_x[i][1] * self.w2 + self.w3
@@ -27,12 +37,10 @@ class BooleanNeural:
 
                 # TODO: check error
                 self.w1 = self.w1 + self.training_nu * (train_answers[i] - out) * out * (1 - out) * \
-                                    train_data_x[i][0]
+                          train_data_x[i][0]
                 self.w2 = self.w2 + self.training_nu * (train_answers[i] - out) * out * (1 - out) * \
-                                    train_data_x[i][1]
+                          train_data_x[i][1]
                 self.w3 = self.w3 + self.training_nu * (train_answers[i] - out) * out * (1 - out)
-
-        return self.w1, self.w2, self.w3
 
     def test(self):
         return self.activate_function(x[0][0] * self.w1 + x[0][1] * self.w2 + self.w3)
