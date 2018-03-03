05cd43e9f140d4e1e463b1b4cd58d776d107c4ba
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-03-03 18:18:14 +0300

    add tests with 2 vars

--- app/test.py
+++ app/test.py
@@ -4,15 +4,37 @@ from neural import BooleanNeural
 from z2 import Z2
 
 
-def model(vars, AND, OR, NOT):
+def model_simple(vars, AND, OR, NOT):
+    return AND(vars[0], vars[1])
+
+
+def model_13(vars, AND, OR, NOT):
     return AND(OR(NOT(vars[0]), OR(NOT(vars[1]), NOT(vars[2]))), OR(NOT(vars[1]), OR(NOT(vars[2]), vars[3])))
 
 
-neural = BooleanNeural(4, Z2(4).truth_table(model), activate_function_hardly, 0.3, 100000)
+class TestTraining(unittest.TestCase):
+    def test_2_vars_true_1_1(self):
+        neural = self.__training__(2, model_simple)
+        self.assertTrue(neural.test([1, 1]) > 0.8)
 
-print('Training...')
-neural.training()
-print('Done')
+    def test_2_vars_false_1_0(self):
+        neural = self.__training__(2, model_simple)
+        self.assertTrue(neural.test([1, 0]) < 0.1)
 
-print('Test')
-print(neural.test([0, 0, 0, 0]))
+    def test_2_vars_false_0_1(self):
+        neural = self.__training__(2, model_simple)
+        self.assertTrue(neural.test([1, 0]) < 0.1)
+
+    def test_2_vars_false_0_0(self):
+        neural = self.__training__(2, model_simple)
+        self.assertTrue(neural.test([0, 0]) < 0.1)
+
+    def __training__(self, vars, model):
+        neural = BooleanNeural(vars, Z2(vars).truth_table(model), activate_function_hardly, 0.3, 100000)
+        neural.training()
+
+        return neural
+
+
+if __name__ == '__main__':
+    unittest.main()
