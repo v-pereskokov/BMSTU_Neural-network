dea40fb0a8de32c25239dea9a2928ec22f8c2544
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-03-03 14:32:12 +0300

    update traner

--- app/neural.py
+++ app/neural.py
@@ -9,34 +9,22 @@ train_answers = y[1:4]
 
 
 class BooleanNeural:
-    variables = None
-    model_function = None
+    vars = 2
+    truth_table = None
     activate_function = None
     training_nu = None
     epoch_number = None
-    truth_table = [[]]
     weights = []
 
-    def __init__(self, variables, activate_function, training_nu=1.0, epoch_number=100):
-        self.variables = variables
+    def __init__(self, vars, truth_table, activate_function, training_nu=1.0, epoch_number=100):
+        self.truth_table = truth_table
         self.activate_function = activate_function
         self.training_nu = training_nu
         self.epoch_number = epoch_number
-
-    def __model__(self, vars):
-        return (not vars[0] or not vars[1] or not vars[2]) and (not vars[1] or not vars[2] or vars[3])
+        self.weights = [0] * vars
 
     def training(self):
-        result = []
-        result[0] = [0] * self.variables
-
-        for step in range(2 ** self.variables):
-            result = 0
-            for vars in result[step]:
-                result[step] = self.__model__(vars)
-                print(result[step])
-
-        # return self.__training__()
+        pass
 
     def __training__(self):
         for epoch in range(self.epoch_number):
