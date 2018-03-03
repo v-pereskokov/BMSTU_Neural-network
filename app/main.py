d6f74c692d4b5a71548535713af20626c79d8995
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-03-03 23:46:58 +0300

    add new route

--- app/main.py
+++ app/main.py
@@ -8,7 +8,24 @@ from z2 import Z2
 app = Flask(__name__)
 
 
-@app.route("/magic", methods=["POST"])
+@app.route("/lazy_magic", methods=["POST"])
+def magic_simple():
+    data = json.loads(request.data)
+    result = neural_simple.test(data["vars"])
+
+    y = result["out"]
+    reality = result["reality"]
+
+    return jsonify({
+        "result": 1 if y > 0.8 else 0,
+        "probability": y,
+        "reality": reality,
+        "vars": data["vars"],
+        "error": y - reality
+    })
+
+
+@app.route("/real_magic", methods=["POST"])
 def magic():
     data = json.loads(request.data)
     result = neural.test(data["vars"])
@@ -30,6 +47,9 @@ def model(vars, AND, OR, NOT):
 
 
 if __name__ == "__main__":
+    neural_simple = BooleanNeural(4, Z2(4).truth_table(model), activate_function_simple, 0.3, 100000)
+    neural_simple.training()
+
     neural = BooleanNeural(4, Z2(4).truth_table(model), activate_function_hardly, 0.3, 100000)
     neural.training()
 
