6ef68d3a27dccd65241605eead429ca58b451f87
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-03-04 10:43:12 +0300

    change epoch numbers

--- app/main.py
+++ app/main.py
@@ -75,10 +75,10 @@ def model(vars, AND, OR, NOT):
 if __name__ == "__main__":
     truth_table_real = Z2(4).truth_table(model)
 
-    neural_simple = BooleanNeural(4, truth_table_real, activate_function_simple, 0.3, 100000)
+    neural_simple = BooleanNeural(4, truth_table_real, activate_function_simple, 0.3, 5000)
     neural_simple.training()
 
-    neural = BooleanNeural(4, truth_table_real, activate_function_hardly, 0.3, 100000)
+    neural = BooleanNeural(4, truth_table_real, activate_function_hardly, 0.3, 5000)
     neural.training()
 
     port = int(os.environ.get('PORT', 5000))
