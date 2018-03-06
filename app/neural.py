6dd1b63d5d54e3827b99c12e78c4d7a547f07fe7
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-03-06 18:28:36 +0300

    update report

--- app/neural.py
+++ app/neural.py
@@ -14,6 +14,9 @@ class BooleanNeural:
         "weights": []
     }
 
+    '''
+        Конструктор для инициализации полей
+    '''
     def __init__(self, vars, truth_table, activate_function, training_nu=1.0, epoch_number=100):
         self.truth_table = truth_table
         self.activate_function = activate_function
@@ -21,6 +24,9 @@ class BooleanNeural:
         self.epoch_number = epoch_number
         self.weights = [0] * (vars + 1)
 
+    '''
+        Проведение теста над переменными
+    '''
     def test(self, vars):
         reality = None
         for row in self.truth_table:
@@ -32,7 +38,7 @@ class BooleanNeural:
             "reality": reality
         }
 
-    def start(self):
+    def min_search(self):
         for i in range(9):
             top = []
             for j in range(9):
@@ -64,6 +70,11 @@ class BooleanNeural:
 
         return self.info if debug else None
 
+    '''
+        Проведение обучения на полной таблице истинности.
+        Таблица истинности генерируется под определенное число переменных 
+        и под нужную булеву функцию
+    '''
     def training(self, debug=False, simple=False):
         for epoch in range(self.epoch_number):
             for i in range(len(self.truth_table)):
@@ -89,6 +100,9 @@ class BooleanNeural:
     def get_info(self):
         return self.info
 
+    '''
+        Вычисление net
+    '''
     def __calculate_net__(self, data):
         net = 0
         for j in range(len(data)):
@@ -96,12 +110,18 @@ class BooleanNeural:
 
         return net + self.weights[len(data)]
 
+    '''
+        Обновление весов
+    '''
     def __update_weights__(self, data, net, out, error, simple):
         for index_weight in range(len(self.weights)):
             self.weights[index_weight] = self.weights[index_weight] + self.training_nu * error * (
                 1 if simple else self.__df__(net)) * (
                                              data[index_weight] if index_weight != len(data) else 1)
 
+    '''
+        Вычисление расстояния Хэмминга
+    '''
     def __calculate_error__(self):
         truth_table = []
         for index in range(len(self.truth_table)):
@@ -116,6 +136,9 @@ class BooleanNeural:
 
         return error
 
+    '''
+        Проверка на значение результата по Y(net)
+    '''
     def __local_test__(self, vars):
         result = self.test(vars)
         y = result["out"]
@@ -125,5 +148,8 @@ class BooleanNeural:
 
         return [except_result, y, reality]
 
+    '''
+        Производная (добавил в этот класс)
+    '''
     def __df__(self, net):
         return 0.5 * (1 / ((abs(net) + 1) ** 2))
