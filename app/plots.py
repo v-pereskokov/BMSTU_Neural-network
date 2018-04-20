ad3e6148fa2de54b54fdac1495c51a6033899010
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-04-20 23:43:31 +0300

    add plot

--- app/plots.py
+++ app/plots.py
@@ -1,15 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from neural import GraphNeural
 
-CORRECT_FUNC = lambda x: np.sin(0.1 * (x ** 3) - 0.2 * (x ** 2) + x - 1)
+correct_func = lambda x: np.sin(0.1 * (x ** 3) - 0.2 * (x ** 2) + x - 1)
 
-t = np.arange(0.0, 7.0, 0.01)
-s = CORRECT_FUNC(t)
-plt.plot(t, s)
 
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('Correct plot')
-plt.grid(True)
-plt.savefig("plots/correct.png")
-plt.show()
+def get_correct_plot():
+    x_vector = np.arange(0, 1, 0.05)
+    y_vector = correct_func(x_vector)
+    plt.plot(x_vector, y_vector)
+
+    plt.title('Correct plot')
+    plt.grid(True)
+    plt.savefig("plots/correct.png")
+    plt.show()
+
+
+def get_training_plot():
+    x_vector = np.arange(0, 1, 1 / 16)
+
+    neural = GraphNeural(0, 1)
+    neural.training()
+    result = neural.test()["training_data"]
+
+    plt.plot(x_vector, result)
+    plt.title('Training plot')
+    plt.grid(True)
+    plt.savefig("plots/training.png")
+    plt.show()
+
+
+# get_correct_plot()
+get_training_plot()
