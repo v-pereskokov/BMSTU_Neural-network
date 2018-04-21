1c43b6972a8dc022fbd1035669d607953a289f5e
vladpereskokov <vladislavpereskokov@yandex.ru>
2018-04-21 09:10:48 +0300

    add error (epoch) plot

--- app/plots.py
+++ app/plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 from neural import GraphNeural
 
 correct_func = lambda x: np.sin(0.1 * (x ** 3) - 0.2 * (x ** 2) + x - 1)
@@ -10,7 +11,7 @@ def get_correct_plot():
     y_vector = correct_func(x_vector)
     plt.plot(x_vector, y_vector)
 
-    plt.title('Correct plot')
+    plt.title("Correct plot")
     plt.grid(True)
     plt.savefig("plots/correct.png")
     plt.show()
@@ -24,11 +25,23 @@ def get_training_plot():
     result = neural.test()["training_data"]
 
     plt.plot(x_vector, result)
-    plt.title('Training plot')
+    plt.title("Training plot")
     plt.grid(True)
     plt.savefig("plots/training.png")
     plt.show()
 
 
+def get_error_epoch_plot():
+    neural = GraphNeural(0, 1)
+    training_result = neural.training()
+
+    plt.plot(np.arange(0, training_result["steps"], 1), training_result["errors"])
+    plt.title("Error(epoch)")
+    plt.grid(True)
+    plt.savefig("plots/error.png")
+    plt.show()
+
+
 # get_correct_plot()
-get_training_plot()
+# get_training_plot()
+get_error_epoch_plot()
